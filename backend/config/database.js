9de5f77d828ab104048cc75ff3fb35ef655a1588
ShahoodZikria96 <imahmadkhan1029@gmail.com>
2025-12-23 17:16:20 +0500

    Set default DB host to localhost

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const dbConfig = {
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'u221106554_root',
   password: process.env.DB_PASSWORD || 'Nono@#696969',
   database: process.env.DB_NAME || 'u221106554_fluent_lol',
