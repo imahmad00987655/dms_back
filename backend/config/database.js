2a5565aa4e7a484c7d0f84827868cd57f5f00e7c
ShahoodZikria96 <imahmadkhan1029@gmail.com>
2025-12-23 16:29:06 +0500

    Update database config defaults for Hostinger

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'u221106554_root',
   password: process.env.DB_PASSWORD || 'Nono@#696969',
   database: process.env.DB_NAME || 'u221106554_fluent_lol',
